fe3f9846f0236f13f99ae61d5607087f291c1dd2
lparolari <luca.parolari23@gmail.com>
2019-02-03 23:07:08 +0100

    Update entry point

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-
 from wpc.cli import cli
 
 if __name__ == "__main__":
     cli.cli_commands()
+
+# this script can be used in IDE (PyCharm) as script path.
+# the shell entry point is defined in setup.py.
--- wpc/shell_cli.py
+++ wpc/shell_cli.py
@@ -3,8 +3,7 @@
 from wpc.cli import cli
 
 if __name__ == "__main__":
-    cli()
-    # cli.cli()
+    cli.cli_commands()
 
     # sys.exit(main())  # pragma: no cover
 
